0dd6b08ff67c90120fc8011402790277971825dc
chinoaladino <chinoaladino@hotmail.com>
2021-07-24 11:08:17 -0400

    subiendo

---
+++ src/main/java/com/apiux_test/springboot/app/configuration/TareaConfig.java
@@ -0,0 +1,33 @@
+package com.apiux_test.springboot.app.configuration;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.apiux_test.springboot.app.entity.Tarea;
+import com.apiux_test.springboot.app.repository.TareaRepository;
+
+@Configuration
+public class TareaConfig {
+	
+	@Bean
+	CommandLineRunner commandLineRunner(TareaRepository repository) {
+		return args -> {
+			Tarea tarea1 = new Tarea(
+					"TAREA 1",
+					false
+				);
+			Tarea tarea2 = new Tarea(
+					"TAREA 2",
+					false
+				);
+			repository.saveAll(
+					List.of(tarea1,tarea2)
+					);
+			
+		};
+	}
+	
+}
